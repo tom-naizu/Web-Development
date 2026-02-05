ce2192d6bf06d7b367258bf8f20dfc8e3e96de42
Tom Naizu...™ <anjalixfr@gmail.com>
2026-02-06 02:13:27 +0530

    JAVA SCRIPT
    JS Code

---
+++ JS/Video_58/index.js
@@ -0,0 +1,31 @@
+function nice(name) {
+    console.log("heyy!! " + name + " this course is nice!")
+    console.log("heyy!! " + name + " this course is nice!")
+    console.log("heyy!! " + name + " this course is nice!")
+    console.log("heyy!! " + name + " this course is nice!")
+    console.log("heyy!! " + name + " this course is nice!")
+}
+
+nice("Tom Naizu")
+
+function sum(a , b , c = 3) {
+    console.log(a + b + c)
+}
+
+result1 = sum(5 , 7)
+result2 = sum(8 , 9)
+result3 = sum(5 , 7 , 1)
+
+console.log("The sum of these number is:",  result1)
+console.log("The sum of these number is:",  result2)
+console.log("The sum of these number is:",  result3)
+
+const func1 = (x)=>{
+    console.log("I am the arrow function",x)
+}
+
+func1(54);
+func1(88);
+func1(56);
+func1(24);
+

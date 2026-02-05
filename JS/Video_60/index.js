544e270be147439e7b0dd4be7ec15a8a17bf7df4
Tom Naizu...™ <anjalixfr@gmail.com>
2026-02-06 03:13:55 +0530

    JAVA SCRIPT
    JS Code

---
+++ JS/Video_60/index.js
@@ -0,0 +1,29 @@
+console.log("This is string Tutorial:")
+let a = "Tom_Naizu";
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+console.log(a[6])
+console.log(a[7])
+console.log(a[8])
+console.log(a[9])
+
+console.log(a.length)
+
+let real_name = "Tom"
+let friend = "Naizu"
+console.log("His name is " + real_name + " and his friend name is " + friend)
+console.log(`His name is ${real_name} and his freind name is ${friend}`)
+
+b = "ShivamSh"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(0,4))
+console.log(b.replace("Sh" , "77"))
+console.log(b.concat(a,"77"))
+
+

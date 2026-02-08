0af6162377ad51c92f13576504561b75bdfdf05b
Tom Naizu...™ <anjalixfr@gmail.com>
2026-02-08 21:07:03 +0530

    JAVA SCRIPT
    JS Code

--- JS/Video_67/index.js
+++ JS/Video_67/index.js
@@ -1,4 +1,21 @@
 console.log("Hello Tom Naizu....")
 
+/*
+Hello Tom Naizu....
 
+document.body
+<body>​…​</body>​
 
+document.body.childNodes
+NodeList(5) [text, div.conatainer, text, script, text]0: text1: div.conatainer2: text3: script4: textlength: 5[[Prototype]]: NodeList
+
+document.body.childNodes[1]
+<div class=​"conatainer">​…​</div>​<div class=​"box">​BOX​</div>​flex<div class=​"box">​BOX​</div>​flex<div class=​"box">​BOX​</div>​flex<div class=​"box">​BOX​</div>​flex<div class=​"box">​BOX​</div>​flex</div>​
+
+document.body.childNodes[1].childNodes
+NodeList(11) [text, div.box, text, div.box, text, div.box, text, div.box, text, div.box, text]
+
+*/
+document.body.firstElementChild
+document.body.firstElementChild.childNodes
+document.body.firstElementChild.children
---
+++ JS/Video_68/index.js
@@ -0,0 +1,18 @@
+console.log("#...Tom Naizu...#")
+
+// let tom_naizus = document.getElementsByClassName("box")
+// console.log(tom_naizus)
+// tom_naizus[2].style.backgroundColor = "green"
+
+// let boxes = document.getElementsByClassName("box")
+// console.log(boxes)
+// boxes[2].style.backgroundColor = "green"    
+
+// document.getElementById("redbox").style.backgroundColor = "yellow"
+// document.querySelector(".box").style.backgroundColor = "red"
+
+document.querySelectorAll('.box').forEach(e => {
+    e.style.backgroundColor = "red";
+});
+console.log(document.querySelectorAll)
+
